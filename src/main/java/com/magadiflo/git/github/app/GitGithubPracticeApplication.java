6a7afa3fc931e1bd6feb3f5dab0b7a57f59dc4da
Martín <magadiflo@gmail.com>
2023-09-19 16:34:39 -0500

    C2 - Segundo commit

--- src/main/java/com/magadiflo/git/github/app/GitGithubPracticeApplication.java
+++ src/main/java/com/magadiflo/git/github/app/GitGithubPracticeApplication.java
@@ -1,13 +1,22 @@
 package com.magadiflo.git.github.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GitGithubPracticeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitGithubPracticeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GitGithubPracticeApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            System.out.println("Iniciando aplicación");
+        };
+    }
 
 }
